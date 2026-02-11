1bca2023b8a95611c18a133896d75cb1b5fe4097
guddn485-afk <guddn485@gmail.com>
2026-02-11 23:01:07 +0900

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,23 +1,22 @@
 import path from 'path';
-import { defineConfig, loadEnv } from 'vite';
+import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-export default defineConfig(({ mode }) => {
-    const env = loadEnv(mode, '.', '');
-    return {
-      server: {
-        port: 3000,
-        host: '0.0.0.0',
-      },
-      plugins: [react()],
-      define: {
-        'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
-        'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY)
-      },
-      resolve: {
-        alias: {
-          '@': path.resolve(__dirname, '.'),
-        }
-      }
-    };
+export default defineConfig({
+  // 1. 여기에 본인의 깃허브 저장소 이름을 넣으세요 (슬래시 필수!)
+  // 예: base: '/my-react-app/',
+  base: '//', 
+
+  plugins: [react()],
+  
+  server: {
+    port: 3000,
+    host: '0.0.0.0',
+  },
+
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'), // 보통 src 폴더를 @로 잡습니다.
+    }
+  }
 });
